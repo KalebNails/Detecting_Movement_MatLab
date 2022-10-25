84b3bb38fc7f61208a1c0b73e34505ac02e59a01
KalebNails <102830532+KalebNails@users.noreply.github.com>
2022-10-25 16:04:22 -0400

    Update greyscalepixelchange.m

--- greyscalepixelchange.m
+++ greyscalepixelchange.m
@@ -1,7 +1,9 @@
 %Kaleb Nails
 %Created: 3/~/2022
 %Modified: 10/25/2022
-%Purpose: put a dot on live video feed where the most change in pixels happen
+%
+%Purpose: put a dot on live video feed where the most change in pixels happen. 
+%This is not optimized nor was it intended to be. 
 
 clear, clc,close;
 load('cameraParams.mat')
@@ -13,8 +15,6 @@ oldUndistorted = undistortImage(oldGreyscaled, cameraParams);
 
 pause(.1)
 
-
-
 for timer = 1:100
 
 rowlocations = 0;
@@ -24,40 +24,21 @@ newimg = snapshot(cam);
 Greyscaled = rgb2gray(newimg);
 Undistorted = undistortImage(Greyscaled, cameraParams);
 
-%[rowlocations, columnlocations] = find(0== Undistorted - oldUndistorted );
+
 difpoints = abs(Undistorted - oldUndistorted);
 
 [rowlocations, columnlocations] = find(110>difpoints & difpoints>70);
 
-
-
-
 axis equal
 axis tight
-%compare = [Undistorted, oldUndistorted];
 
-
-%imshow(compare);
 imshow(Undistorted)
 
 hold on
 plot(round(mean(columnlocations)),round(mean(rowlocations)),'r*','MarkerSize',16);
 oldUndistorted = Undistorted;
 
-
-
-
-
-
 pause(.1)
 end
 hold on
-%plot(rowlocations,columnlocations,'b*','MarkerSize',1);
 
-%{
-for r = 1:50:length(columnlocations)
-plot(columnlocations(r,1),rowlocations(r,1),'b*','MarkerSize',2);
-hold on
-
-end
-%}
